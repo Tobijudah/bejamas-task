3b33b4cd14616473159cf90d9fd344c021c5c1d9
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-28 15:52:51 +0100

    feat: add description and favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="description" content="Sell your Images and Artworks" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700&display=swap" rel="stylesheet" />
