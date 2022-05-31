1eb9e1a74fe730123693c611b7dd9583129ba8bf
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-31 11:24:45 +0100

    style: make header sticky & full width

--- components/Header/Header.styles.ts
+++ components/Header/Header.styles.ts
@@ -3,10 +3,19 @@ import Logo from "../../public/svgs/logo";
 import breakpoints from "../../styles/breakpoints";
 
 export const Container = styled.header`
+  position: sticky;
+  top: 0;
+  width: 100vw;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 2rem 0;
+  background-color: #fff;
+  padding: 2rem 5%;
+  z-index: 1;
+
+  ${breakpoints.mobile} {
+    padding: 2rem 1rem;
+  }
 `;
 
 export const LogoIcon = styled(Logo)`
