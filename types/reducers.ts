85448418b75b08fb66c3e5c569c507bb8c1929fc
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-31 08:54:57 +0100

    chore: add reducer types

---
+++ types/reducers.ts
@@ -0,0 +1,10 @@
+import { Reducer } from "react";
+import { ReducerActions } from "./actions";
+import { GlobalState } from "./globalState";
+import { NonFeaturedProduct, Product } from "./product";
+
+export type MainReducer = Reducer<GlobalState, ReducerActions>;
+
+export type ProductsReducer = Reducer<NonFeaturedProduct[], ReducerActions>;
+
+export type CartReducer = Reducer<Product[], ReducerActions>;
