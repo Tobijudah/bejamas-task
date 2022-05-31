6e37c3469a67a91feee62685d160af059c01a034
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-31 18:35:24 +0100

    feat: add pagination to Products

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -1,10 +1,11 @@
 import Filter from "../Filter/Filter";
 import Product from "../Product/Product";
 import MobileModal from "../Modal/MobileModal";
-import React, { useEffect, useState } from "react";
+import Pagination from "../Pagination/Pagination";
 import SortIcon from "../../public/svgs/sort-icon";
 import useMobileOnly from "../../hooks/useMobileOnly";
 import { ProductListProps } from "./ProductList.types";
+import React, { useEffect, useRef, useState } from "react";
 import DropdownIcon from "../../public/svgs/dropdown-icon";
 import {
   Products,
@@ -17,19 +18,33 @@ import {
   ListSortItem,
   GreyListHeading,
   StyledFilterIcon,
+  ProductsContainer,
   ListHeadingDivider,
 } from "./ProductList.styles";
 
 const ProductList: React.FC<ProductListProps> = ({ products }) => {
   const isMobile = useMobileOnly();
+  const ref = useRef<HTMLDivElement>(null);
   const [display, setDisplay] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const productsPerPage = 6;
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
   useEffect(() => {
     if (!isMobile) setDisplay(false);
   }, [isMobile]);
 
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [currentPage]);
+
   return (
-    <Container>
+    <Container ref={ref}>
       {isMobile && (
         <MobileModal display={display} onClose={() => setDisplay(false)}>
           <Filter />
@@ -52,11 +67,12 @@ const ProductList: React.FC<ProductListProps> = ({ products }) => {
       <ListBody>
         <ListFilter />
         <ProductsContainer>
-        <Products>
-          {products.map((product, index) => (
-            <Product key={index} {...product} />
-          ))}
-        </Products>
+          <Products>
+            {currentProducts.map((product, index) => (
+              <Product key={index} {...product} />
+            ))}
+          </Products>
+          <Pagination currentPage={currentPage} perPage={productsPerPage} total={products.length} paginate={paginate} />
         </ProductsContainer>
       </ListBody>
     </Container>
