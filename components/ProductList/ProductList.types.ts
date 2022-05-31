fed03d999f899297c194be903d08b3535fffad50
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-31 11:28:29 +0100

    chore: update import path

--- components/ProductList/ProductList.types.ts
+++ components/ProductList/ProductList.types.ts
@@ -1,3 +1,3 @@
-import { NonFeaturedProduct } from "../../types/types";
+import { NonFeaturedProduct } from "../../types/product";
 
 export type ProductListProps = { products: NonFeaturedProduct[] };
