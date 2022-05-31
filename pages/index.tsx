bba7bec521f9dd02be52e4934bab2b125d1061b8
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-31 11:33:18 +0100

    feat: add props and data to index route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import data from "../data";
 import { NextPage } from "next";
 import Home from "../views/Home/Home";
+import { HomeProps } from "../views/Home/Home.types";
+import { FeaturedProduct, NonFeaturedProduct } from "../types/product";
 
-const Index: NextPage = () => {
-  return <Home />;
+export const getServerSideProps = async () => {
+  // simulate fetching data from a database
+  const response = data;
+
+  // get products (non-featured)
+  const products = response.products.filter((product): product is NonFeaturedProduct => product.featured === false);
+
+  // get featured product
+  const featuredProduct = response.products.find((product) => product.featured === true) as FeaturedProduct;
+
+  return {
+    props: { featuredProduct, products },
+  };
+};
+
+const Index: NextPage<HomeProps> = (props) => {
+  return <Home {...props} />;
 };
 
 export default Index;
