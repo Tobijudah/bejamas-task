6eec1da5ea5ff46e293b16f71c306af627f655cd
Oluwatobiju Judah <tobijudah@gmail.com>
2022-05-31 08:55:44 +0100

    chore: add reducers

---
+++ context/mainReducer.ts
@@ -0,0 +1,31 @@
+import { CartReducer, MainReducer, ProductsReducer } from "../types/reducers";
+
+const cartReducer: CartReducer = (state, { type, data }) => {
+  switch (type) {
+    case "ADD_TO_CART": {
+      return [...state, data];
+    }
+    case "CLEAR_CART": {
+      return [];
+    }
+    default:
+      return state;
+  }
+};
+
+const productsReducer: ProductsReducer = (state, { type, data }) => {
+  switch (type) {
+    case "UPDATE_PRODUCTS": {
+      return data;
+    }
+    default:
+      return state;
+  }
+};
+
+const mainReducer: MainReducer = ({ cart, products }, action) => ({
+  cart: cartReducer(cart, action),
+  products: productsReducer(products, action),
+});
+
+export default mainReducer;
